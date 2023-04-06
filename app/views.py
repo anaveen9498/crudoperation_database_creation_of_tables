40bfd2329dd48395645c92f598b8d5b224646924
anaveen9498 <anaveen9498@gmail.com>
2023-04-06 17:54:25 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from app.models import *
+from django.db.models.functions import Length
 
 # Create your views here.
 def databasetable(request):
     TOB=Topic.objects.all()
+    TOB=Topic.objects.all()[:2:]
+    TOB=Topic.objects.all().order_by('topic_name')
+    TOB=Topic.objects.all().order_by('-topic_name')
+    TOB=Topic.objects.all().order_by(Length('topic_name'))
+    TOB=Topic.objects.all().order_by(Length('topic_name').desc())
     d={'table':TOB}
     return render(request,'databasetable.html',d)
 
@@ -16,5 +22,10 @@ def webtable(request):
 
 def displaylocation(request):
     LOLO=Location.objects.all()
+    LOLO=Location.objects.filter(age__gt=22)
+    LOLO=Location.objects.filter(age__gte=25)
+    LOLO=Location.objects.filter(age__lt=25)
+    LOLO=Location.objects.filter(age__lte=25)
+
     d={'name':LOLO}
     return render(request,'displaylocation.html',d)
